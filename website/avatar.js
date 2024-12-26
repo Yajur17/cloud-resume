1cb658613e78d52f634c32552d8c2eee9dfc5df1
Yajur17 <87426903+Yajur17@users.noreply.github.com>
2024-12-26 07:52:28 +0530

    Update avatar.js

--- website/avatar.js
+++ website/avatar.js
@@ -56,7 +56,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-const counter = document.querySelector(".counter-number");
+const counter = document.querySelectorAll(".counter-number");
 async function updateCounter() {
     let response = await fetch(
         "https://2nfo3hb4svry26aqbg4ysd7t5i0mqwdf.lambda-url.ap-south-1.on.aws/"
